e7c401f8336d059c42eb104b91b41b198d3e6282
Pham Thu Thao <thaochanchan2000@gmail.com>
2024-10-14 10:02:17 +0900

    Commit files to create app image

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=master
+export POSTGRES_PASSWORD=phamthao1910
+export POSTGRES_HOST=database-pro3.c1nrskrc11hk.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::bucket-project3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
